db56a04b02b6975bde42a715edba0084f8c1bdc8
Johnathan May <mtjmay@gmail.com>
2023-07-22 11:33:38 -0600

    fix install_common_binaries and allow force reinstall

--- bin/install_common_binaries.sh
+++ bin/install_common_binaries.sh
@@ -1,6 +1,20 @@
 #!/bin/sh
 
-arch=`arch`
+TMP_DIR="/tmp"
+INITIAL_DIR=$(pwd)
+LOG_FILE=$HOME/.install_common_binaries.log
+
+echo $(date +%Y%m%d%H%M) >> $LOG_FILE
+
+notice() {
+    echo "\n####################\n# $1\n####################\n"
+}
+
+if [ "$1" = "-f" ]; then
+    FORCE="true"
+fi
+
+arch=$(arch)
 if [ "$arch" = "amd64" ] || [ "$arch" = "x86_64" ]; then
     url_aws="https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip"
     url_kubectl="https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
@@ -12,7 +26,7 @@ else
 fi
 
 # install [arkade](https://github.com/alexellis/arkade)
-if ! type arkade; then
+if ! type arkade || $FORCE = "true"; then
     notice "INSTALL arkade"
     curl -sLS https://get.arkade.dev | sh
     mv arkade $HOME/bin
@@ -22,26 +36,30 @@ if ! type arkade; then
 fi
 
 # install AWS CLI
-if ! type aws; then
+if ! type aws || $FORCE = "true"; then
     notice "INSTALL aws cli v2"
     curl -sL "$url_aws" -o "${TMP_DIR}/awscliv2.zip"
     cd $TMP_DIR
     unzip -q awscliv2.zip
+    AWS_VER=$(./aws/dist/aws --version |awk '{print $1}' |awk -F/ '{print $2}')
     sudo ./aws/install --update >> $LOG_FILE
+    aws --version >> $LOG_FILE
     rm -rf ./awscliv2.zip ./aws
     cd $INITIAL_DIR
 fi
 
 # install kubectl
-if ! type kubectl; then
+if ! type kubectl || $FORCE = "true"; then
     notice "INSTALL kubectl"
     curl -sL "$url_kubectl" -o "kubectl"
     sudo install kubectl /usr/local/bin/kubectl
+    chmod +x ./kubectl
+    ./kubectl version >> $LOG_FILE
     rm kubectl
 fi
 
 # install yq
-if ! type yq; then
+if ! type yq || $FORCE = "true"; then
     notice "INSTALL yq"
     curl -sL "$url_yq" -o "${TMP_DIR}/yq"
     chmod 755 "${TMP_DIR}/yq"
